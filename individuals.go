4287cb8e88bc93ba21cf3524219de900525a3bf0
paul-hoehne <paul.hoehne@gmail.com>
2018-09-04 01:14:20 -0400

    Adding individuals

--- individuals.go
+++ individuals.go
@@ -1,7 +1,10 @@
 package ea
 
 import (
+	"math/rand"
 	"sync"
+
+	"github.com/paul-hoehne/ea/alleles"
 
 	"github.com/paul-hoehne/ea/genes"
 )
@@ -24,17 +27,74 @@ type IndividualFactory struct {
 	GeneFactories []genes.GeneFactory
 }
 
-// Spawn is asexual reproduction.
-func (i Individual) Spawn() Individual {
-	return Individual{}
+// Create a new individual
+func (i IndividualFactory) Create() Individual {
+	result := Individual{
+		Genes: make([]genes.Gene, len(i.GeneFactories)),
+	}
+
+	for i, f := range i.GeneFactories {
+		result.Genes[i] = genes.Gene{
+			Alleles: make([]alleles.Allele, len(f.AlleleFactories)),
+		}
+
+		for j, af := range f.AlleleFactories {
+			result.Genes[i].Alleles[j] = af.Random()
+		}
+	}
+
+	return result
 }
 
-// Breed is sexual reproduction
-func (i Individual) Breed(crossoverRate float64) Individual {
-	return Individual{}
+// Spawn is asexual reproduction.
+func (i Individual) Spawn() Individual {
+	result := Individual{
+		Genes: make([]genes.Gene, len(i.Genes)),
+	}
+
+	for idx, g := range i.Genes {
+		result.Genes[idx] = genes.Gene{
+			Alleles: make([]alleles.Allele, len(g.Alleles)),
+		}
+
+		for j, a := range g.Alleles {
+			result.Genes[idx].Alleles[j] = a
+		}
+	}
+
+	return result
+}
+
+// Breed is sexual reproduction.  Note that crossover is done
+// at the gene level.  Since our genes can be code values or real
+// numbers as well as bit strings, bit by bit crossover is ill
+// defined for this implementation (and may be handled as an optimized
+// bitstring individual later)
+func (i Individual) Breed(other Individual, crossoverRate float64) Individual {
+	result := Individual{
+		Genes: make([]genes.Gene, len(i.Genes)),
+	}
+
+	for idx, g := range i.Genes {
+		result.Genes[idx].Alleles = make([]alleles.Allele, len(g.Alleles))
+		for j, a := range g.Alleles {
+			if rand.Float64() < crossoverRate {
+				result.Genes[idx].Alleles[j] = other.Genes[idx].Alleles[j]
+			} else {
+				result.Genes[idx].Alleles[j] = a
+			}
+		}
+	}
+
+	return result
 }
 
 // Mutate mutates an individual
-func (i *Individual) Mutate(mutationRate float64) {
+func (i *Individual) Mutate(muts []genes.GeneMutator, mutationRate float64) {
+	for idx, g := range i.Genes {
 
+		if rand.Float64() < mutationRate {
+			i.Genes[idx] = muts[idx].Mutate(g)
+		}
+	}
 }
---
+++ individuals_test.go
@@ -0,0 +1,264 @@
+package ea
+
+import (
+	"testing"
+
+	"github.com/paul-hoehne/ea/alleles"
+	"github.com/paul-hoehne/ea/genes"
+)
+
+func TestIndividualFactoryCreate(t *testing.T) {
+	ifact := IndividualFactory{
+		GeneFactories: []genes.GeneFactory{
+			genes.GeneFactory{
+				AlleleFactories: []alleles.Factory{
+					alleles.BitFactory{Width: 4, OnFrequency: 1.0},
+				},
+			},
+			genes.GeneFactory{
+				AlleleFactories: []alleles.Factory{
+					alleles.BitFactory{Width: 4, OnFrequency: 1.0},
+					alleles.CodeFactory{Codes: []byte{2}},
+				},
+			},
+		},
+	}
+
+	i := ifact.Create()
+
+	if len(i.Genes) != 2 {
+		t.Errorf("Expected 2 genes but got %d", len(i.Genes))
+	}
+
+	if len(i.Genes[0].Alleles) != 1 || len(i.Genes[1].Alleles) != 2 {
+		t.Errorf("Expected 1 allele in the first gene and 2 in the second but got: %d and %d",
+			len(i.Genes[0].Alleles), len(i.Genes[1].Alleles))
+	}
+
+	if _, ok := i.Genes[0].Alleles[0].(alleles.BitAllele); !ok {
+		t.Error("Expected first allele to be a bit Allele")
+	}
+
+	if _, ok := i.Genes[1].Alleles[0].(alleles.BitAllele); !ok {
+		t.Error("Expected second allele to be a bit allele")
+	}
+
+	if _, ok := i.Genes[1].Alleles[1].(alleles.CodeAllele); !ok {
+		t.Error("Expected third allele to be a code allele")
+	}
+}
+
+func TestIndividualSpawn(t *testing.T) {
+	i := Individual{
+		Genes: []genes.Gene{
+			genes.Gene{
+				Alleles: []alleles.Allele{
+					alleles.BitAllele{Width: 4, Bits: []byte{1}},
+				},
+			},
+			genes.Gene{
+				Alleles: []alleles.Allele{
+					alleles.RealAllele(0.5),
+				},
+			},
+			genes.Gene{
+				Alleles: []alleles.Allele{
+					alleles.CodeAllele{Value: 1},
+				},
+			},
+			genes.Gene{
+				Alleles: []alleles.Allele{
+					alleles.RealAllele(0.1),
+					alleles.BitAllele{Width: 6, Bits: []byte{5}},
+					alleles.CodeAllele{Value: 7},
+				},
+			},
+		},
+	}
+
+	ix := i.Spawn()
+
+	if len(ix.Genes) != 4 {
+		t.Errorf("Expected 4 genes but got %d", len(ix.Genes))
+	}
+
+	if len(ix.Genes[0].Alleles) != 1 || len(ix.Genes[1].Alleles) != 1 ||
+		len(ix.Genes[2].Alleles) != 1 || len(ix.Genes[3].Alleles) != 3 {
+		t.Errorf("Expected 1, 1, 1, and 3 alleles but got %d, %d, %d and %d",
+			len(ix.Genes[0].Alleles), len(ix.Genes[1].Alleles),
+			len(ix.Genes[2].Alleles), len(ix.Genes[3].Alleles))
+	}
+
+	if _, ok := ix.Genes[0].Alleles[0].(alleles.BitAllele); !ok {
+		t.Error("Expected first allele to be a bit allele")
+	}
+
+	if _, ok := ix.Genes[1].Alleles[0].(alleles.RealAllele); !ok {
+		t.Error("Expected second allele to be a real allele")
+	}
+
+	if _, ok := ix.Genes[2].Alleles[0].(alleles.CodeAllele); !ok {
+		t.Errorf("Expected third allele to be a code allele")
+	}
+
+	if _, ok := ix.Genes[3].Alleles[0].(alleles.RealAllele); !ok {
+		t.Error("Expected fourth allele to be a real allele")
+	}
+
+	if _, ok := ix.Genes[3].Alleles[1].(alleles.BitAllele); !ok {
+		t.Error("Expected fifth allele to be a bit allele")
+	}
+
+	if _, ok := ix.Genes[3].Alleles[2].(alleles.CodeAllele); !ok {
+		t.Errorf("Expected sixth allele to be a code allele")
+	}
+}
+
+func TestIndividualBreed(t *testing.T) {
+	p1 := Individual{
+		Genes: []genes.Gene{
+			genes.Gene{
+				Alleles: []alleles.Allele{
+					alleles.BitAllele{Width: 4, Bits: []byte{1}},
+				},
+			},
+			genes.Gene{
+				Alleles: []alleles.Allele{
+					alleles.CodeAllele{Value: 4},
+				},
+			},
+		},
+	}
+
+	p2 := Individual{
+		Genes: []genes.Gene{
+			genes.Gene{
+				Alleles: []alleles.Allele{
+					alleles.BitAllele{Width: 4, Bits: []byte{10}},
+				},
+			},
+			genes.Gene{
+				Alleles: []alleles.Allele{
+					alleles.CodeAllele{Value: 1},
+				},
+			},
+		},
+	}
+
+	p3 := p1.Breed(p2, 1.0)
+
+	if len(p3.Genes) != 2 {
+		t.Errorf("Expected child to have 2 genes but got: %d", len(p3.Genes))
+	}
+
+	if len(p3.Genes[0].Alleles) != 1 || len(p3.Genes[1].Alleles) != 1 {
+		t.Errorf("Expected each gene to have 1 allele but got %d and %d",
+			len(p3.Genes[0].Alleles), len(p3.Genes[0].Alleles))
+	}
+
+	b, ok := p3.Genes[0].Alleles[0].(alleles.BitAllele)
+	if !ok {
+		t.Errorf("Expected the first alleles of the child to be a bit allele")
+	}
+
+	if b.Bits[0] != 10 {
+		t.Errorf("Expected crossover value to be 10 but got %d", b.Bits[0])
+	}
+
+	c, ok := p3.Genes[1].Alleles[0].(alleles.CodeAllele)
+	if !ok {
+		t.Errorf("Expected the second allele of the child to be a code allele")
+	}
+
+	if c.Value != 1 {
+		t.Errorf("Expected the second crossover value to be 1 but got: %d", c.Value)
+	}
+
+	p3 = p1.Breed(p2, 0.0)
+
+	b, ok = p3.Genes[0].Alleles[0].(alleles.BitAllele)
+	if b.Bits[0] != 1 {
+		t.Errorf("Expected crossover value to be 1 but got %d", b.Bits[0])
+	}
+
+	c, ok = p3.Genes[1].Alleles[0].(alleles.CodeAllele)
+	if c.Value != 4 {
+		t.Errorf("Expected crossover value to be 4 but got %d", c.Value)
+	}
+}
+
+func TestIndividualMutate(t *testing.T) {
+	p1 := Individual{
+		Genes: []genes.Gene{
+			genes.Gene{
+				Alleles: []alleles.Allele{
+					alleles.BitAllele{Width: 4, Bits: []byte{8}},
+				},
+			},
+			genes.Gene{
+				Alleles: []alleles.Allele{
+					alleles.CodeAllele{Value: 4},
+				},
+			},
+		},
+	}
+
+	ms := []genes.GeneMutator{
+		genes.GeneMutator{
+			AlleleMutators: []alleles.Mutator{
+				alleles.BitMutator{MutationRate: 1.0},
+			},
+		},
+		genes.GeneMutator{
+			AlleleMutators: []alleles.Mutator{
+				alleles.CodeMutator{Codes: []byte{1, 2, 3}},
+			},
+		},
+	}
+
+	p1.Mutate(ms, 1.0)
+
+	b, ok := p1.Genes[0].Alleles[0].(alleles.BitAllele)
+	if !ok {
+		t.Error("Expected first allele to be a bit allele")
+	}
+
+	if b.Bits[0] != 0x7 {
+		t.Errorf("Expected mutated bits to be 0x0c but got %x", b.Bits[0])
+	}
+
+	c, ok := p1.Genes[1].Alleles[0].(alleles.CodeAllele)
+	if !ok {
+		t.Error("Expected second allele to be a code allele")
+	}
+
+	if c.Value == 4 {
+		t.Error("Expected mutated value to not be 4")
+	}
+
+	p1 = Individual{
+		Genes: []genes.Gene{
+			genes.Gene{
+				Alleles: []alleles.Allele{
+					alleles.BitAllele{Width: 4, Bits: []byte{8}},
+				},
+			},
+			genes.Gene{
+				Alleles: []alleles.Allele{
+					alleles.CodeAllele{Value: 4},
+				},
+			},
+		},
+	}
+
+	p1.Mutate(ms, 0.0)
+	b = p1.Genes[0].Alleles[0].(alleles.BitAllele)
+	if b.Bits[0] != 8 {
+		t.Errorf("Expected mutated bits to be 8, but got %x", b.Bits[0])
+	}
+
+	c = p1.Genes[1].Alleles[0].(alleles.CodeAllele)
+	if c.Value != 4 {
+		t.Errorf("Expected value to be four but got %d", c.Value)
+	}
+}
